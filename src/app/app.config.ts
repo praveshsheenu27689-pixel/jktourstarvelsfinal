25c6d776080c498d90851539007a4fe5a491e628
Pravesh Kumar <praveshsheenu27689@gmail.com>
2026-03-29 22:24:53 +0530

    initial push

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,22 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
 import { provideHttpClient } from '@angular/common/http';
 import { withHashLocation } from '@angular/router';
-=======
->>>>>>> 18aafcba21d516c54cdc1d453584d2eb2657d085
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-<<<<<<< HEAD
     provideRouter(routes, withHashLocation()),
     provideAnimations(),
     provideHttpClient()
-=======
-    provideRouter(routes),
-    provideAnimations()
->>>>>>> 18aafcba21d516c54cdc1d453584d2eb2657d085
   ]
 };
--- src/app/components/booking/booking.component.ts
+++ src/app/components/booking/booking.component.ts
@@ -1,11 +1,8 @@
 import { Component, Input, OnChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-<<<<<<< HEAD
 import { HttpClientModule } from '@angular/common/http';
 import { ApiService } from '../../core/services/api.service';
-=======
->>>>>>> 18aafcba21d516c54cdc1d453584d2eb2657d085
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
@@ -14,11 +11,7 @@ gsap.registerPlugin(ScrollTrigger);
 @Component({
   selector: 'app-booking',
   standalone: true,
-<<<<<<< HEAD
   imports: [CommonModule, FormsModule, HttpClientModule],
-=======
-  imports: [CommonModule, FormsModule],
->>>>>>> 18aafcba21d516c54cdc1d453584d2eb2657d085
   templateUrl: './booking.component.html',
   styleUrls: ['./booking.component.scss']
 })
@@ -26,7 +19,6 @@ export class BookingComponent implements OnChanges {
   @Input() prefilledPackage = '';
 
   submitted = false;
-<<<<<<< HEAD
   loading = false;
   form = {
     name: '', email: '', phone: '',
@@ -36,14 +28,6 @@ export class BookingComponent implements OnChanges {
 
   constructor(private api: ApiService) {}
 
-=======
-  form = {
-    name: '', email: '', phone: '',
-    destination: '', date: '', travelers: '2',
-    budget: '', message: '', packageName: ''
-  };
-
->>>>>>> 18aafcba21d516c54cdc1d453584d2eb2657d085
   ngOnChanges() {
     if (this.prefilledPackage) {
       this.form.packageName = this.prefilledPackage;
@@ -52,7 +36,6 @@ export class BookingComponent implements OnChanges {
   }
 
   submit() {
-<<<<<<< HEAD
     this.loading = true;
     this.api.submitBooking(this.form).subscribe({
       next: () => {
@@ -70,20 +53,10 @@ export class BookingComponent implements OnChanges {
         }, 10);
       }
     });
-=======
-    this.submitted = true;
-    setTimeout(() => {
-      gsap.fromTo('.success-msg', { scale: 0.8, opacity: 0 }, { scale: 1, opacity: 1, duration: 0.5, ease: 'back.out(1.7)' });
-    }, 10);
->>>>>>> 18aafcba21d516c54cdc1d453584d2eb2657d085
   }
 
   reset() {
     this.submitted = false;
-<<<<<<< HEAD
     this.form = { name: '', email: '', phone: '', destination: '', travelDate: '', travelers: '2', budget: '', message: '', packageName: '' };
-=======
-    this.form = { name: '', email: '', phone: '', destination: '', date: '', travelers: '2', budget: '', message: '', packageName: '' };
->>>>>>> 18aafcba21d516c54cdc1d453584d2eb2657d085
   }
 }
